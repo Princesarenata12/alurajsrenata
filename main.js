0a6041f60d1d8ba20cff6636185c9f22debaf310
Princesarenata12 <115024737+Princesarenata12@users.noreply.github.com>
2023-09-18 12:40:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function pom () {
+alert("botao pom clicado");
+}
+document.querySelector('.tecla_pom').onclick = pom;
+
+function clap(){
+  alert ("botao clap clicado")
+}
+document.querySelector('.tecla_clap').onclick = clap;
